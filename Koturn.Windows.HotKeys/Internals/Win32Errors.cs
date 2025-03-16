6331dc357b82393041658fe8025d97041350169a
koturn <jeak.koutan.apple@gmail.com>
2025-03-16 19:09:05 +0900

    Initial commit

---
+++ Koturn.Windows.HotKeys/Internals/Win32Errors.cs
@@ -0,0 +1,13 @@
+namespace Koturn.Windows.HotKeys.Internals
+{
+    /// <summary>
+    /// Win32 error codes used in this library.
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes"/>
+    /// </remarks>
+    internal enum Win32Errors
+    {
+        InsufficientBuffer = 122
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Koturn.Windows.HotKeys
+======================
+
+Library to handle global hot keys.
+
+## LICENSE
+
+This software is released under the MIT License, see [LICENSE](LICENSE "LICENSE").
